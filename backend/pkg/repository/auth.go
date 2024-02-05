45f8adc45bff6fa9466c1cfc4819f643b5fad697
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2024-02-06 00:34:14 +0200

    feat: sign-in & sign up endpoints

--- backend/pkg/repository/auth.go
+++ backend/pkg/repository/auth.go
@@ -1,8 +1,17 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"errors"
 
-type AuthRepository interface{}
+	"auction-system/pkg/models"
+
+	"gorm.io/gorm"
+)
+
+type AuthRepository interface {
+	CreateUser(user *models.User) (err error)
+	GetUserByUsername(username string) (*models.User, error)
+}
 
 type repo struct {
 	db *gorm.DB
@@ -10,4 +19,28 @@ type repo struct {
 
 func NewAuthRepository(db *gorm.DB) AuthRepository {
 	return &repo{db: db}
+}
+
+func (db *repo) CreateUser(user *models.User) (err error) {
+	tx := db.db.Begin()
+
+	if err = tx.Create(user).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
+}
+
+func (db *repo) GetUserByUsername(username string) (*models.User, error) {
+	user := new(models.User)
+
+	err := db.db.Where("username = ?", username).First(user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return user, nil
 }
