0f888ec2fb61137f8f2dfcfdb1c77e27ed06d0e5
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2024-02-06 00:41:00 +0200

    fix: remove constraint from the fullName

--- backend/migrations/00001_init_users.up.sql
+++ backend/migrations/00001_init_users.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username    VARCHAR(255) not null unique,
-    fullName    varchar(255) not null unique,
+    fullName    varchar(255) not null,
     password    varchar(255) not null,
     createdAt   timestamptz DEFAULT now()
 )
