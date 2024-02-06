eb71413710826248247dd8b46defab0c8d786f7a
Kolia913 <mynew1mail470@gmail.com>
2024-02-06 22:40:05 +0200

    fix: fixed dockerfile

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-grey': '#272D2D',
+        'darkest-grey': '#222727',
+        milky: '#DFDFD7',
+        'light-milky': '#EAEAE4',
+        white: '#F1F1EE',
+        grey: '#585855',
+        'light-green': '#77FF61',
+      },
+    },
   },
   plugins: [],
 };
